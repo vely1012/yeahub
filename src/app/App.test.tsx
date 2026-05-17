3927be4121dd4059c4ee39368591fe890226228a
vely1012 <damyanovv228666@gmail.com>
2026-05-17 21:20:00 +0300

    minor import fix

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,8 +1,8 @@
 /** @jest-environment jsdom */
 
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
 describe('App', () => {
   it('renders', () => {
